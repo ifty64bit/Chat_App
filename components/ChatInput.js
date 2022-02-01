823bae2335fc4d59d85373f91ac81acab132fbd3
Ifty Islam <ifty64bit@gmail.com>
2022-02-01 13:30:49 +0600

    fix input

--- components/ChatInput.js
+++ components/ChatInput.js
@@ -5,19 +5,22 @@ import { IconContext } from "react-icons";
 function ChatInput({ sendMsg }) {
     const [input, setInput] = useState("");
 
-    const send = () => {
+    const send = (e) => {
+        e.preventDefault();
         sendMsg(input);
         setInput("");
     }
 
     return (
-        <div className='px-6 grow flex gap-x-4 text-black fixed bottom-4 w-[80%] lg:w-[75%] 2xl:w-[70%]'>
-            <input className='w-full rounded-lg px-2 drop-shadow-md' type="text" onChange={(e) => setInput(e.target.value)} value={ input } />
-            <IconContext.Provider value={{ color: "black", size: '2rem' }}>
-                <div className=' bg-green-400 p-3 rounded-full flex justify-center items-center shadow-md cursor-pointer' onClick={ send }>
-                    <FiSend />
-                </div>
-            </IconContext.Provider>
+        <div className='px-6 grow fixed bottom-4 w-[80%] lg:w-[75%] 2xl:w-[70%]'>
+            <form className='flex gap-x-4 text-black' action="" onSubmit={send}>
+                <input className='w-full rounded-lg px-2 drop-shadow-md' type="text" onChange={(e) => setInput(e.target.value)} value={ input } />
+                <IconContext.Provider value={{ color: "black", size: '2rem' }}>
+                    <div className=' bg-green-400 p-3 rounded-full flex justify-center items-center shadow-md cursor-pointer' onClick={ send }>
+                        <FiSend />
+                    </div>
+                </IconContext.Provider>
+            </form>
         </div>
     )
 }
